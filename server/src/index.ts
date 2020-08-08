66424b930cc25b94f4313e603d701f06bece9596
DmytroIv <dmytropopovkh@gmail.com>
2020-08-08 11:17:56 +0300

    done simple server with graphQl and Apollo

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+require('dotenv').config();
+//
+import express, {Application} from 'express';
+import {ApolloServer} from 'apollo-server-express';
+//
+import {connectDatabase} from './database';
+import {resolvers, typeDefs} from './graphql';
+
+const mount = async (app: Application) => {
+
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({db})
+  });
+  server.applyMiddleware({app, path: '/api'});
+
+  app.listen(process.env.PORT);
+};
+
+mount(express());
