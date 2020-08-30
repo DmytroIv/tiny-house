bfa5052e14b3636ea71af57ac66bdc2100be0591
DmytroIv <dmytropopovkh@gmail.com>
2020-08-30 19:44:02 +0300

    client part, login (Google OAuth 2), user, navigation components server updated (login, user)

---
+++ server/temp/clear.ts
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+import { connectDatabase } from '../src/database';
+
+
+const clear = async () => {
+  try {
+    console.log('[clear] : running');
+
+    const db = await connectDatabase();
+
+    const bookings = await db.bookings.find({}).toArray();
+    const users = await db.users.find({}).toArray();
+    const listings = await db.listings.find({}).toArray();
+
+    if (bookings.length) {
+      await db.bookings.drop();
+    }
+
+    if (users.length) {
+      await db.users.drop();
+    }
+
+    if (listings.length) {
+      await db.listings.drop();
+    }
+
+    console.log('[clear] : success');
+  } catch (error) {
+    throw new Error('Failed to seed db.');
+  }
+};
+
+clear();
+
